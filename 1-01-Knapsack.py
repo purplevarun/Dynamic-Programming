4b66a0c4bd1a564abb582ce4c85a8e90109cb00a
purplevarun <purplevarun@gmail.com>
2021-04-06 14:49:56 +0530

    .

--- 1-01-Knapsack.py
+++ 1-01-Knapsack.py
@@ -1,10 +1,23 @@
-'''
-    Given : Weight [], Value [] and Max Capacity
-    Output : Maximum Value
+# 
+#     Given : Weight [], Value [] and Max Capacity
+#     Output : Maximum Value
 
-    Checking for DP:
-    1. It has a choice (either choose item or don't)
-    2. We need optimal solution
+#     Checking for DP:
+#     1. It has a choice (either choose item or don't)
+#     2. We need optimal solution
 
-    Therefore, it is DP.
-'''
+#     Therefore, it is DP.
+# 
+# 
+#          APPROACH :
+
+#    W = Weight of Item, C = Max Capacity
+
+#            Item
+#            /  \
+#           /    \
+#         W<=C   W>C
+#         / \     |  
+#        /   \    |
+#       1     0   0 
+
