6cf9be04a5fd474b41ad0184feacf2ad9839bf02
purplevarun <purplevarun@gmail.com>
2021-04-06 15:08:38 +0530

    .

---
+++ 1-01-Knapsack.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+#define vi vector <int>
+/* 
+    Given : Weight [], Value [] and Max Capacity
+    Output : Maximum Value
+
+    Checking for DP:
+    1. It has a choice (either choose item or don't)
+    2. We need optimal solution
+
+    Therefore, it is DP.
+ 
+          APPROACH :
+
+    W = Weight of Item, C = Max Capacity
+
+            Item
+            /  \
+           /    \
+         W<=C   W>C
+         / \     |  
+        /   \    |
+       1     0   0 
+
+ Base Condition : Think of smallest valid input
+*/
+
+int32_t main () {
+
+}
