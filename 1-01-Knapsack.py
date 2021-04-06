536b08d539f98566810c3adb77795836b763cb90
purplevarun <purplevarun@gmail.com>
2021-04-06 14:56:24 +0530

    .

--- 1-01-Knapsack.py
+++ 1-01-Knapsack.py
@@ -20,3 +20,9 @@
 #        /   \    |
 #       1     0   0 
 
+# Base Condition : Think of smallest valid input
+def Knapsack (Weights,Values,Capacity):
+    N = len (Weights)
+    if N == 0 or Capacity == 0:
+        return 0
+    
