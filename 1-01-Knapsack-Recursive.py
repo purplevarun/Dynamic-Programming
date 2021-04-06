7f0b12a7c300d005eaff140ae0922e8b9d69e3e3
purplevarun <purplevarun@gmail.com>
2021-04-06 18:09:18 +0530

    .

--- 1-01-Knapsack-Recursive.py
+++ 1-01-Knapsack-Recursive.py
@@ -20,21 +20,26 @@
 #        1     0   0 
 
 #  Base Condition : Think of smallest valid input
-
+dp = {}
 def Knapsack (wt,val,N,W):
+    key = str(N) + "," + str(W)
+    if key in dp: return dp[key]
     if N == 0 or W == 0:
         return 0
     
     if wt[N-1] <= W:
-        return max (
+        dp[key] =  max (
             val[N-1] + Knapsack(wt,val,N-1,W-wt[N-1]),
             Knapsack(wt,val,N-1,W)
         )
+        return dp[key]
     else :
-        return Knapsack(wt,val,N-1,W)
+        dp[key] = Knapsack(wt,val,N-1,W)
+        return dp[key]
 
 wt = [10,20,30]
 val = [60,100,120]
 W = 50
 N = 3
 print (Knapsack(wt,val,N,W))
+print (dp)
