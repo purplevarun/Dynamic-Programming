f08ed3874cf66489f6bbd401e2d5f7a7683c35b2
purplevarun <purplevarun@gmail.com>
2021-04-06 14:35:38 +0530

    .

---
+++ 1-01-Knapsack.py
@@ -0,0 +1,10 @@
+'''
+    Given : Weight [], Value [] and Max Capacity
+    Output : Maximum Value
+
+    Checking for DP:
+    1. It has a choice (either choose item or don't)
+    2. We need optimal solution
+
+    Therefore, it is DP.
+'''
