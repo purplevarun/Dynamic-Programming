d24b69ff4b55e86f3daf271026bec1693a8d9211
purplevarun <purplevarun@gmail.com>
2021-04-07 12:48:50 +0530

    .

--- 1-01-Knapsack-Tabulation.cc
+++ 1-01-Knapsack-Tabulation.cc
@@ -2,19 +2,27 @@
 using namespace std;
 #define int int64_t
 int KnapSack (int W, int *wt, int *val, int N){
-    int dp[W+1][N+1];
+    int dp[N+1][W+1];
     // base cases
-    for (int i=0;i<W+1;i++){
+    for (int i=0;i<N+1;i++){
         dp[i][0] = 0;
     }
-    for (int j=0;j<N+1;j++){
-        dp[0][i] = 0;
+    for (int j=0;j<W+1;j++){
+        dp[0][j] = 0;
     }
-    for (int i=1;i<W+1;i++){
-        for (int j=1;j<N+1;j++){
-            
+    for (int i=1;i<N+1;i++){
+        for (int j=1;j<W+1;j++){
+            if (wt[i-1]<=j){
+                dp[i][j] = max (
+                    val[i-1] + dp[i-1][j-wt[i-1]],
+                    dp[i-1][j]
+                );
+            } 
+            else
+                dp[i][j] = dp[i-1][j];
         }
     }
+    return dp[N][W];
 }
 int32_t main () {
     int W = 50;
