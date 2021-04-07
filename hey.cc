a932b3ed3641ffd65c41ecc94c49dbabf9bf936c
purplevarun <purplevarun@gmail.com>
2021-04-07 15:37:18 +0530

    .

--- hey.cc
+++ hey.cc
@@ -1,9 +1,9 @@
-// https://www.hackerrank.com/contests/srin-aadc03/challenges/classic-01-knapsack/problem
-// All TC accepted
 #include <bits/stdc++.h>
 using namespace std;
 #define int int64_t
-int KnapSack (int W, int *wt, int *val, int N){
+int W,N;
+// unordered_map <string,int> dp;
+int knap (int W,int *wt, int *val, int N){
     int dp[N+1][W+1];
     // base cases
     for (int i=0;i<N+1;i++){
@@ -20,18 +20,24 @@ int KnapSack (int W, int *wt, int *val, int N){
                     dp[i-1][j]
                 );
             } 
-            else{
+            else
                 dp[i][j] = dp[i-1][j];
-            }
         }
     }
     return dp[N][W];
 }
-
-int32_t main () {
-    int W = 50;
-    int wt[] = {10,20,30};
-    int val[] = {60,100,120};
-    int N = 3;
-    cout << KnapSack(W,wt,val,N) << endl;
+void solve () {
+    cin >> W >> N;
+    int wt[N],val[N];
+    for (int i=0;i<N;i++)
+        cin >> wt[i] >> val[i];
+    cout << knap (W,wt,val,N) << endl;
+}
+int32_t main() {
+    clock_t start = clock();
+    int T; cin >> T;
+    while (T--) solve ();
+    clock_t end = clock();
+    // cerr << "TIME = " << fixed<<setprecision(5)<<double(end-start)/double(CLOCKS_PER_SEC);
+    return 0;
 }
