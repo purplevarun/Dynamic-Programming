855b0ecab03cc511ac5d4529aa789c008d43449b
purplevarun <purplevarun@gmail.com>
2021-04-08 18:53:02 +0530

    .

--- 2-Subset-Sum.cc
+++ 2-Subset-Sum.cc
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int int64_t
-
+bool canSum (int *A, int N, int Sum){
+    
+}
 int32_t main () {
     int N = 5;
     int A = {2,3,7,8,10};
     int Sum = 11;
-    cout << canSum (A,Sum) << endl;
+    cout << canSum (A, N, Sum) << endl;
 }
