fdaa5bcaa76b599ae4ea2d2799f518f13c8457b2
purplevarun <purplevarun@gmail.com>
2021-04-08 18:52:12 +0530

    .

--- 2-Subset-Sum.cc
+++ 2-Subset-Sum.cc
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int int64_t
+
 int32_t main () {
-    
+    int N = 5;
+    int A = {2,3,7,8,10};
+    int Sum = 11;
+    cout << canSum (A,Sum) << endl;
 }
