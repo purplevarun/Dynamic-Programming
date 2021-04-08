cd39a96542d5220b04e1903c20bf724f08f23e75
purplevarun <purplevarun@gmail.com>
2021-04-08 19:08:11 +0530

    .

---
+++ 2-Subset-Sum-Tabulation.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+bool canSum (int *A, int N, int Sum){
+    
+}
+int32_t main () {
+    int A[] = {7,14};
+    int N = sizeof(A)/sizeof(A[0]);
+    int Sum = 300;
+    cout << canSum (A, N, Sum) << endl;
+}
