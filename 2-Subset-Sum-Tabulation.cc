5ab58824438326a207515639714ec5982e215c16
purplevarun <purplevarun@gmail.com>
2021-04-09 03:33:43 +0530

    .

--- 2-Subset-Sum-Tabulation.cc
+++ 2-Subset-Sum-Tabulation.cc
@@ -1,12 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int int64_t
+int dp[10007][10007];
 bool canSum (int *A, int N, int Sum){
-    int dp[N+1][Sum+1];
+    // dp[N+1][Sum+1]
     // base cases
     // when sum is 0
-    for ()
+    for (int i=0;i<N+1;i++){
+        dp[i][0] = true;
+    }
     // when array size is 0
+    for (int j=0;j<Sum+1;j++){
+        dp[0][j] = false;
+    }
+    
 }
 int32_t main () {
     int A[] = {7,14};
