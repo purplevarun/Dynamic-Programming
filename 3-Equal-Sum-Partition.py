f2e70ae335684bf7c420aeab97d1157422af7327
purplevarun <purplevarun@gmail.com>
2021-04-10 01:27:18 +0530

    .

--- 3-Equal-Sum-Partition.py
+++ 3-Equal-Sum-Partition.py
@@ -1,10 +1,11 @@
-bool subsetSum (A,Sum):
+def subsetSum (A,Sum):
     N = len (A)
+    dp = ([None]*(N+1))*(Sum+1)
     
-bool EqualSumPartition (A):
+def EqualSumPartition (A):
     if sum(A)%2!=0:
         return False
     return subsetSum(A,sum(A)//2)
 
-A = [1,5,11,5]
+A = [1,5,11,1111]
 print (EqualSumPartition(A))
