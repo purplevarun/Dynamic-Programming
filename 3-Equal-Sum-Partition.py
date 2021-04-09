506e2173aa2637b699f6793f676b25a4f5bb2954
purplevarun <purplevarun@gmail.com>
2021-04-09 23:37:18 +0530

    .

--- 3-Equal-Sum-Partition.py
+++ 3-Equal-Sum-Partition.py
@@ -1,1 +1,11 @@
+from time import time
+start = time()
 A = [1,5,11,5]
+for i in range (10**8):
+    x = 10
+end = time()
+X = int(input())
+if X%2 == 0 and X>2:
+    print ("YES")
+else : print ("NO")
+
