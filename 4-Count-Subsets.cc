ab64935e2545b400379955a1b9072da5bcb89577
purplevarun <purplevarun@gmail.com>
2021-04-10 15:40:36 +0530

    .

--- 4-Count-Subsets.cc
+++ 4-Count-Subsets.cc
@@ -4,7 +4,7 @@ using namespace std;
 #define vi vector <int>
 #define int int64_t
 int countSubsets (int *A, int N, int Sum ){
-    return 10;
+    return 1000;
 }
 int32_t main () {
     int A[] = {2,3,5,6,8,10};
