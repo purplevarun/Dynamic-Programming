650945ec0ca8c37b5a0fda00690d864abc42bfe8
purplevarun <purplevarun@gmail.com>
2021-04-10 22:48:09 +0530

    .

--- 5-Minimum-Subset-Sum-Difference.cc
+++ 5-Minimum-Subset-Sum-Difference.cc
@@ -1,8 +1,11 @@
 // https://practice.geeksforgeeks.org/problems/minimum-sum-partition3317/1 
 #include <bits/stdc++.h>
 using namespace std;
+bool isSubsetSum (int *A, int N, int Sum){
+	
+}
 int MinSubsetDiff (int *A, int N){
-									
+							
 	return 1;
 }
 int main () {
