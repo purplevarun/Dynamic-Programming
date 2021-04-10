aac2188db045797cad278a0acffe0cd4544dbe08
purplevarun <purplevarun@gmail.com>
2021-04-10 15:34:06 +0530

    .

--- 4-Count-Subsets.cc
+++ 4-Count-Subsets.cc
@@ -3,11 +3,12 @@
 using namespace std;
 #define vi vector <int>
 #define int int64_t
-
+int countSubsets (int *A, int N, int Sum ){
+    return 10;
+}
 int32_t main () {
     int A[] = {2,3,5,6,8,10};
     int N = 6;
     int Sum = 10;
     cout << countSubsets(A,N,Sum) << endl;
-    a
 }
---
+++ yo.py
@@ -0,0 +1,3 @@
+for i in range (10**8):
+    sums = 10
+print (sums**13) 
