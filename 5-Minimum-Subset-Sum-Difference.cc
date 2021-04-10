7b5ccf209927e65c9190b6a61cbd785cc3cb6bc3
purplevarun <purplevarun@gmail.com>
2021-04-10 23:42:48 +0530

    .

--- 5-Minimum-Subset-Sum-Difference.cc
+++ 5-Minimum-Subset-Sum-Difference.cc
@@ -1,15 +1,41 @@
 // https://practice.geeksforgeeks.org/problems/minimum-sum-partition3317/1 
 #include <bits/stdc++.h>
 using namespace std;
-bool isSubsetSum (int *A, int N, int Sum){
-	
+vector<bool> SubsetSum (int *A, int N, int Sum){
+	bool dp[N+1][Sum+1];
+	for (int i=0;i<N+1;i++){
+		for (int j=0;j<Sum+1;j++){
+			if (i == 0)
+				dp[i][j] = false;
+			if (j == 0)
+				dp[i][j] = true;
+		}
+	}	
+	dp[0][0] = true;
+	for (int i=1;i<N+1;i++) {
+		for (int j=1;j<Sum+1;j++){
+			if (A[i-1]<=j)
+				dp[i][j] = dp[i-1][j] || dp[i-1][j-A[i-1]];
+			else	
+				dp[i][j] = dp[i-1][j];
+		}
+	}
+	vector<bool> ans;
+	for (int i=0;i<(Sum+1)/2;i++){
+		ans.push_back(dp[N][i]);
+	}
+	return ans;
 }
 int MinSubsetDiff (int *A, int N){
-							
+	vector <bool> ans = SubsetSum(A,N,accumulate(A,A+N,0));
+	vector<int> idx;
+	for (int i=0;i<ans.size();i++) 
+		if (ans[i]) idx.push_back(i);
+	
 	return 1;
 }
 int main () {
-	int A[] = {1,6,11,5};
-	int N = 4;
+	int A[] = {1,2,7};
+	int N = 3;
 	cout << MinSubsetDiff (A,N) << endl;
 }
