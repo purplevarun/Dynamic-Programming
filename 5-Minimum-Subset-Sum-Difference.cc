38f0d9e90f929fafa47a745adc71142f23ca0974
purplevarun <purplevarun@gmail.com>
2021-04-10 19:19:57 +0530

    .

---
+++ 5-Minimum-Subset-Sum-Difference.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int MinSubsetDiff (int *A, int N){
+	
+}
+int main () {
+	int A[] = {1,5,11,5};
+	int N = 4;
+	cout << MinSubsetDiff (A,N) << endl;
+}
