fac9eff922bb8b724b6e6b477a5b4be6da97623f
purplevarun <purplevarun@gmail.com>
2021-04-11 03:36:39 +0530

    .

--- 6-Count-Subsets-with-given-difference.cc
+++ 6-Count-Subsets-with-given-difference.cc
@@ -16,7 +16,15 @@ int countSubsets (int *A, int N, int Sum) {
     }
     dp[0][0] = 1;
     // fill dp
-    
+    for (int i=1;i<N+1;i++){
+        for (int j=1;j<Sum+1;j++){
+            if (A[i-1]<=j)
+                dp[i][j] = dp[i-1][j] + dp[i-1][j-A[i-1]];
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+    }
+    return dp[N][Sum];
 }
 /*
     S1 = subset 1
@@ -30,7 +38,7 @@ int countSubsets (int *A, int N, int Sum) {
     Also , sum(S2) = (sum(A) - D)/2
 */
 int countSubsetsDifference (int *A, int N, int D){
-
+    
 }
 int32_t main () {
     int A[] = {1,1,2,3};
