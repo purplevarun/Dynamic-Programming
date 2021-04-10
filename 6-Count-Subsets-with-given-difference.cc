d458154bf0997add6a3cb88b0f763845db7ee848
purplevarun <purplevarun@gmail.com>
2021-04-11 03:30:31 +0530

    .

--- 6-Count-Subsets-with-given-difference.cc
+++ 6-Count-Subsets-with-given-difference.cc
@@ -2,8 +2,17 @@
 using namespace std;
 #define vi vector <int>
 #define int int64_t
+/*
+    S1 = subset 1
+    S2 = subset 2
+    A = array
+    given :  1) sum(S1) - sum(S2) = D
+    we know, 2) sum(S1) + sum(S2) = sum(A)
+    therefore, 2sum(S1) = D + sum(A)
+              = sum(S1) = (D + sum(A))/2
+*/
 int countSubsetsDifference (int *A, int N, int D){
-    
+
 }
 int32_t main () {
     int A[] = {1,1,2,3};
