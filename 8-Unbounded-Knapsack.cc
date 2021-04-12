43c2e50dca4b76730c8061111d35d12f8e51916a
purplevarun <purplevarun@gmail.com>
2021-04-12 16:35:58 +0530

    .

--- 8-Unbounded-Knapsack.cc
+++ 8-Unbounded-Knapsack.cc
@@ -3,12 +3,27 @@ using namespace std;
 #define vi vector <int>
 #define int int64_t
 int unboundedKnapSack(int W, int wt[], int val[], int N){
-    
+    int dp[N+1][W+1];
+    for (int i=0;i<N+1;i++){
+        for (int j=0;j<W+1;j++){
+            if (i==0){
+                dp[i][j] = 0;
+            }
+            if (j==0){
+                dp[i][j] = 0;
+            }
+        }
+    }
+    for (int i=0;i<N+1;i++){
+        for (int j=0;j<W+1;j++){
+            
+        }
+    }
 }
 int32_t main () {
     int W = 100;
     int wt[] = {1,50};
     int val[] = {1,30};
     int N = 2;
-    cout << unboundedKnapSack(int W, int wt[], int val[], int N) << endl;
+    cout << unboundedKnapSack(W, wt, val, N) << endl;
 }
