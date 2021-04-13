c7657c4c01732ca4aa4bfbdc089afd8197916004
purplevarun <purplevarun@gmail.com>
2021-04-14 03:17:34 +0530

    .

--- 9-Rod-Cutting-Problem.cc
+++ 9-Rod-Cutting-Problem.cc
@@ -4,7 +4,10 @@ using namespace std;
 #define vi vector <int>
 #define int int64_t
 int rodCut (int *Price, int N) {
-
+    int W = N;
+    int length[N];
+    for (int i=0;i<N;i++) length[i] = i+1;
+    
 }
 int32_t main () {
     int Price[] = {1, 5, 8, 9, 10, 17, 17, 20};
