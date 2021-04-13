065a2d828f2b43762df0c9b974a63a379226286c
purplevarun <purplevarun@gmail.com>
2021-04-14 03:15:16 +0530

    .

---
+++ 9-Rod-Cutting-Problem.cc
@@ -0,0 +1,8 @@
+// https://practice.geeksforgeeks.org/problems/rod-cutting0840/1
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <int>
+#define int int64_t
+int32_t main () {
+    
+}
