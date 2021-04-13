53a2e926074bd567d38a4508e5762fee2fbd6261
purplevarun <purplevarun@gmail.com>
2021-04-13 19:46:42 +0530

    .

--- 8-Unbounded-Knapsack.cc
+++ 8-Unbounded-Knapsack.cc
@@ -14,11 +14,18 @@ int unboundedKnapSack(int W, int wt[], int val[], int N){
             }
         }
     }
-    for (int i=0;i<N+1;i++){
-        for (int j=0;j<W+1;j++){
-            
+    for (int i=1;i<N+1;i++){
+        for (int j=1;j<W+1;j++){
+            if (wt[i-1]<=j){
+                dp[i][j] = max ( 
+                    dp[i-1][j],
+                    val[i-1] + dp[i][j-wt[i-1]]
+                );
+            }
+            else dp[i][j] = dp[i-1][j];
         }
     }
+    return dp[N][W];
 }
 int32_t main () {
     int W = 100;
