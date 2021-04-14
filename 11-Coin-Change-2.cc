0d212c545ae45c9ac5f193af2ff63b8dcd921c96
purplevarun <purplevarun@gmail.com>
2021-04-14 13:46:01 +0530

    .

--- 11-Coin-Change-2.cc
+++ 11-Coin-Change-2.cc
@@ -3,7 +3,7 @@ using namespace std;
 #define vi vector <int>
 #define int int64_t
 int minCoins(int *Coins, int N, int Sum){
-    
+    int dp[N+1][Sum+1];
 }
 int32_t main () {
     int coins[] = {1,2,3};
