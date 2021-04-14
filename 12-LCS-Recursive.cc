d9406e426af0502cc0e1dbb6b7909450d321e619
purplevarun <purplevarun@gmail.com>
2021-04-14 16:43:29 +0530

    .

--- 12-LCS-Recursive.cc
+++ 12-LCS-Recursive.cc
@@ -3,7 +3,15 @@ using namespace std;
 #define vi vector <int>
 #define int int64_t
 int LCS (string X, string Y, int N, int M) {
-
+    if (M == 0 || N == 0)
+        return 0;
+    if (X[M-1] == Y[N-1])
+        return LCS(X,Y,N-1,M-1) + 1;
+    
+    return max(
+        LCS(X,Y,M,N-1) ,
+        LCS(X,Y,M-1,N)
+    );
 }
 int32_t main () {
     string X = "ABCDGH";
