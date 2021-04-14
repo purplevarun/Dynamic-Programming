b41724a11b7d54e89aa9c7b0e548b84e08693b86
purplevarun <purplevarun@gmail.com>
2021-04-14 15:26:29 +0530

    .

--- 11-Coin-Change-2.cc
+++ 11-Coin-Change-2.cc
@@ -4,6 +4,7 @@ using namespace std;
 #define int int64_t
 int minCoins(int *Coins, int N, int Sum){
     int dp[N+1][Sum+1];
+    // initialization
     for (int i=0;i<N+1;i++){
         for (int j=0;j<Sum+1;j++){
             if (i==0) // there are no coins
@@ -18,11 +19,23 @@ int minCoins(int *Coins, int N, int Sum){
             }
         }
     }    
-    
+    for (int i=2;i<N+1;i++){
+        for (int j=1;j<Sum+1;j++){
+            if (Coins[i-1]<=j){
+                dp[i][j] = min (
+                    1 + dp[i][j-Coins[i-1]],
+                    dp[i-1][j]
+                );
+            }
+            else 
+                dp[i][j] = dp[i-1][j];
+        }
+    }
+    return dp[N][Sum];
 }
 int32_t main () {  
     int coins[] = {1,2,3};
     int N = 3;
     int Sum = 5;
-    cout << minCoins(coins[],N,Sum) << endl;
+    cout << minCoins(coins,N,Sum) << endl;
 }
