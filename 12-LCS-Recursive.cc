68dc4c2959f46972e647d7d2dbc792d2436eae79
purplevarun <purplevarun@gmail.com>
2021-04-14 16:21:48 +0530

    .

--- 12-LCS-Recursive.cc
+++ 12-LCS-Recursive.cc
@@ -2,11 +2,11 @@
 using namespace std;
 #define vi vector <int>
 #define int int64_t
-int LCS (string X, string Y) {
-    
+int LCS (string X, string Y, int N, int M) {
+
 }
 int32_t main () {
     string X = "ABCDGH";
     string Y = "AEDFHR";
-    cout << LCS (X, Y) << endl;
+    cout << LCS (X, Y, X.size(), Y.size()) << endl;
 }
