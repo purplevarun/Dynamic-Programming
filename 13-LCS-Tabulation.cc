452fcaea3a086e2983d74b4e7f35e2304efdf0e8
purplevarun <purplevarun@gmail.com>
2021-04-14 22:17:30 +0530

    .

--- 13-LCS-Tabulation.cc
+++ 13-LCS-Tabulation.cc
@@ -3,6 +3,15 @@ using namespace std;
 #define vi vector <int>
 #define int int64_t
 int LCS (string X, string Y, int N, int M) {
+    int dp[N+1][M+1];
+    // base case
+    for (int i=0;i<N+1;i++){
+        for (int j=0;j<Sum+1;j++){
+            // base case
+            if (i==0 || j==0)
+                dp[i][j] = 0;
+        }
+    }
     
 }
 int32_t main () {
