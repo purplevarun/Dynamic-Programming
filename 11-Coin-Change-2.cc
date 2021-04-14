4619a8264b04d11ccf02e36988b1366de64b9e99
purplevarun <purplevarun@gmail.com>
2021-04-14 15:40:03 +0530

    .

--- 11-Coin-Change-2.cc
+++ 11-Coin-Change-2.cc
@@ -11,17 +11,13 @@ int minCoins(int *Coins, int N, int Sum){
                 dp[i][j] = 1e9; // hence it will take infinite coins
             if (j==0) // reqd sum is 0
                 dp[i][j] = 0; // 0 coins reqd to get 0
-            if (i==1){
-                if (j%Coins[i-1]==0)
-                    dp[i][j] = j/Coins[i-1];
-                else
-                    dp[i][j] = 1e9;
-            }
         }
     }    
-    for (int i=2;i<N+1;i++){
+    dp[0][0] = 1e9;
+        
+    for (int i=1;i<N+1;i++){
         for (int j=1;j<Sum+1;j++){
-            if (Coins[i-1]<=j){
+            if (Coins[i-1] <= j){
                 dp[i][j] = min (
                     1 + dp[i][j-Coins[i-1]],
                     dp[i-1][j]
@@ -31,11 +27,13 @@ int minCoins(int *Coins, int N, int Sum){
                 dp[i][j] = dp[i-1][j];
         }
     }
+    if (dp[N][Sum] == 1e9)
+        return -1;
     return dp[N][Sum];
 }
 int32_t main () {  
-    int coins[] = {1,2,3};
+    int coins[] = {25,10,5};
     int N = 3;
-    int Sum = 5;
+    int Sum = 30;
     cout << minCoins(coins,N,Sum) << endl;
 }
