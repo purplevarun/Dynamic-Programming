492ec9abd096421b0d8b993d8b447583ca78f8c1
purplevarun <purplevarun@gmail.com>
2021-04-16 16:21:10 +0530

    .

---
+++ 14-Longest-Common-Substring.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <int>
+#define int int64_t
+int32_t main () {
+    string X = "ABCDGH";
+    string Y = "AEDFHR";
+    cout << LCS (X, Y, X.size(), Y.size()) << endl;
+}
