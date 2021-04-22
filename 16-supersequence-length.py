96079a7e0e24afce45247944d5f15fbcfc279b74
purplevarun <purplevarun@gmail.com>
2021-04-22 20:14:02 +0530

    .

--- 16-supersequence-length.py
+++ 16-supersequence-length.py
@@ -1,4 +1,9 @@
+def LCS (A,B):
+    N = len(A); M = len(B)
+    dp = [[0 for i in range (M+1)] for j in range (N+1)]
 
+def supersequence_length(A,B):
+    N = len(A); M = len(B)
 A = "AGGTAB"
 B = "GXTXAYB"
 print (supersequence_length(A,B))
