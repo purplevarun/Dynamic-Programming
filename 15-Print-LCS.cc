ab3ff27cf22ff58e5d13acdd4dab545da3234638
purplevarun <purplevarun@gmail.com>
2021-04-22 15:41:32 +0530

    .

--- 15-Print-LCS.cc
+++ 15-Print-LCS.cc
@@ -3,7 +3,28 @@
 using namespace std;
 vector<int> printLCS (vector<int> A, vector<int> B){
     int N = A.size(), M = B.size();
-     
+    int dp[N+1][M+1];
+    for (int i=0;i<N+1;i++){
+        dp[i][0] = 0;
+    }
+    for (int j=0;j<M+1;j++){
+        dp[0][j] = 0;
+    }
+    vector<int> answer;
+    for (int i=1;i<N+1;i++){
+        for (int j=1;j<M+1;j++){
+            if (A[i-1]==B[j-1]){
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }
+            else {
+                dp[i][j] = max (dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+    int i = N, j = M;
+    while (i>=0 && j>=0){
+        
+    }
 }
 int main () {
     vector <int> A = {1,2,3,4,1};
