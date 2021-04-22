5e1929272df445c67c6ef85a4f997b0848b30538
purplevarun <purplevarun@gmail.com>
2021-04-22 12:37:05 +0530

    .

---
+++ 15-Print-LCS.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printLCS (vector<int> A, vector<int> B){
+    int N = A.size(), M = B.size();
+
+}
+int main () {
+    vector <int> A = {1,2,3,4,1};
+    vector <int> B = {3,4,1,2,1,3};
+    printLCS (A,B);
+}
