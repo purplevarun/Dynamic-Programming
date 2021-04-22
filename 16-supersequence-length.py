2521399fc2e086c0bbcf17650ae2624626498ca8
purplevarun <purplevarun@gmail.com>
2021-04-22 23:21:39 +0530

    .

--- 16-supersequence-length.py
+++ 16-supersequence-length.py
@@ -8,11 +8,24 @@ def LCS (A,B):
             else :
                 dp[i][j] = max (dp[i-1][j], dp[i][j-1])
     i = N; j = M
+    answer = ""
     while i>=0 and j>=0:
         if A[i-1]==B[j-1]:
-            
+            answer+=A[i-1]
+            i-=1
+            j-=1
+        else :
+            if dp[i][j-1]>dp[i-1][j]:
+                j-=1
+            else :
+                i-=1
+    answer=answer[::-1]
+    return len(answer)
+
 def supersequence_length(A,B):
     N = len(A); M = len(B)
+    return N+M-LCS(A,B)
 A = "AGGTAB"
 B = "GXTXAYB"
 print (supersequence_length(A,B))
+# print (LCS(A,B))
