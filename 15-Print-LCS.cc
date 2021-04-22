f7e5fe5407326782d47081530217126a8344f7c1
purplevarun <purplevarun@gmail.com>
2021-04-22 12:38:46 +0530

    .

--- 15-Print-LCS.cc
+++ 15-Print-LCS.cc
@@ -1,11 +1,16 @@
+// https://www.hackerrank.com/challenges/dynamic-programming-classics-the-longest-common-subsequence/problem
 #include <bits/stdc++.h>
 using namespace std;
-void printLCS (vector<int> A, vector<int> B){
+vector<int> printLCS (vector<int> A, vector<int> B){
     int N = A.size(), M = B.size();
-
+     
 }
 int main () {
     vector <int> A = {1,2,3,4,1};
     vector <int> B = {3,4,1,2,1,3};
-    printLCS (A,B);
+    vector <int> Result = printLCS (A,B);
+    for (int i:Result){
+        cout << i << " ";
+    }
+    cout << "\n";
 }
