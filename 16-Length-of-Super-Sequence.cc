9c9c2ec9c0ba959d4927b30faa3c2265499a3b6a
purplevarun <purplevarun@gmail.com>
2021-04-22 23:24:01 +0530

    .

--- 16-Length-of-Super-Sequence.cc
+++ 16-Length-of-Super-Sequence.cc
@@ -1,13 +1,19 @@
 #include <bits/stdc++.h>
-using namespace std:
-string LCS (string A, string B) {
-    int N = A.size(), B.size(); 
-    string lcs = "";
-        
-}
+using namespace std;
+int LCS (string A, string B) {
+    int N = A.size(),M = B.size(); 
+    int dp [N+1][M+1]={{0}};
+    int Max = -1e9;
+    for (int i=1;i<N+1;i++){
+        for (int j=1;j<M+1;j++){
+            
+        }
+    }
+    return 0;    
+}   
 int superSequence(string A, string B) {
-    int N = A.size(), B.size(); 
-    
+    int N = A.size(),M = B.size(); 
+    return 100;
 }
 int main () {
     string A = "AGGTAB";
