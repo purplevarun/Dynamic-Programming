87ff659eb844e64898420e200c20c079b44f803a
purplevarun <purplevarun@gmail.com>
2021-04-26 02:01:21 +0530

    .

--- 17-Min-Num-of-Insertion-Deletion-String-A-to-B.cc
+++ 17-Min-Num-of-Insertion-Deletion-String-A-to-B.cc
@@ -9,14 +9,21 @@ int LCS (string A, string B){
 				dp[i][j] = 0;
 		}
 	}
+
 	for (int i=1;i<N+1;i++){
 		for (int j=1;j<M+1;j++){
-			
+			if (A[i-1] == B[j-1])
+				dp[i][j] = dp[i-1][j-1] + 1;
+			else 
+				dp[i][j] = max (
+					dp[i-1][j], dp[i][j-1]
+				);
 		}
 	}	
+	return dp[N][M];
 }
 int count (string A, string B) {
-	return 100;
+	return A.size() + B.size() - 2*LCS(A,B);
 }
 int main () {
     string A = "heap";
