0b0e0cd6b23bed49cc9ad39e64454e2158fe19e3
purplevarun <purplevarun@gmail.com>
2021-04-28 11:13:38 +0530

    .

--- 18-Longest-Palindromic-Subsequence.cc
+++ 18-Longest-Palindromic-Subsequence.cc
@@ -2,7 +2,20 @@
 using namespace std;
 #define int int64_t
 int LCS (string A, string B) {
-    
+    int N = A.size(), M = B.size();
+    int dp[N+1][M+1];
+    for (int i=0;i<N+1;i++){
+        for (int j=0;j<M+1;j++){
+            if (i==0 || j==0)
+                dp[i][j] = 0;
+        }
+    }
+    for (int i=0;i<N+1;i++){
+        for (int j=0;j<M+1;j++){
+            if (i==0 || j==0)
+                dp[i][j] = 0;
+        }
+    }
 }
 int LPS (string A) {
     string B = A;
