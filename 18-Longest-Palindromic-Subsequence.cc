32e01f7162092bd644cf285f1dcdfc98275c4679
purplevarun <purplevarun@gmail.com>
2021-04-28 11:11:46 +0530

    .

--- 18-Longest-Palindromic-Subsequence.cc
+++ 18-Longest-Palindromic-Subsequence.cc
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int int64_t
+int LCS (string A, string B) {
+    
+}
+int LPS (string A) {
+    string B = A;
+    reverse (B.begin(),B.end());
+    return LCS (A,B);
+}
 int32_t main (){
     string A = "bbbab";
+    cout << LPS (A) << endl;
 }
