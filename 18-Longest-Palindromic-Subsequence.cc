852f96d7a8d553abda97bf786adee59704741881
purplevarun <purplevarun@gmail.com>
2021-04-28 11:14:56 +0530

    .

--- 18-Longest-Palindromic-Subsequence.cc
+++ 18-Longest-Palindromic-Subsequence.cc
@@ -10,12 +10,18 @@ int LCS (string A, string B) {
                 dp[i][j] = 0;
         }
     }
-    for (int i=0;i<N+1;i++){
-        for (int j=0;j<M+1;j++){
-            if (i==0 || j==0)
-                dp[i][j] = 0;
+    for (int i=1;i<N+1;i++){
+        for (int j=1;j<M+1;j++){
+            if (A[i-1] == B[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
+            else 
+                dp[i][j] = max (
+                    dp[i-1][j],
+                    dp[i][j-1]
+                );
         }
     }
+    return dp[N][M];
 }
 int LPS (string A) {
     string B = A;
