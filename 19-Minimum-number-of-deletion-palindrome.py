8e492efe70c4adf509325b46faa8348e9ea4471a
purplevarun <purplevarun@gmail.com>
2021-05-06 21:37:57 +0530

    19-done

--- 19-Minimum-number-of-deletion-palindrome.py
+++ 19-Minimum-number-of-deletion-palindrome.py
@@ -1,9 +1,11 @@
-import numpy as np
 def lcs(A,B):
 	N = len(A)
 	M = len(B)
-	dp = [[0 for i in range (M+1)] for j in range (N+1)]
-	dp = np.array(dp)
+	dp = [[None for i in range (M+1)] for j in range (N+1)]
+	for i in range (N+1):
+		for j in range (M+1):
+			if i == 0 or j == 0:
+				dp[i][j] = 0
 	for i in range (1,N+1):
 		for j in range (1,M+1):
 			if A[i-1] == B[j-1]:
