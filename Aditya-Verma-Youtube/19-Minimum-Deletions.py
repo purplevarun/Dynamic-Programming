7e5e0be1c401557d82f6456c66af1e4ac527e6d4
purplevarun <purplevarun@gmail.com>
2021-05-09 23:12:58 +0530

    min-deletions

--- Aditya-Verma-Youtube/19-Minimum-Deletions.py
+++ Aditya-Verma-Youtube/19-Minimum-Deletions.py
@@ -4,10 +4,17 @@ def LCS (A,B):
 	M = len(B)
 	dp = [[0 for i in range (M+1)] for j in range (N+1)]
 	for i in range (1,N+1):
-		for j in range (1,N+1):
-
+		for j in range (1,M+1):
+			if A[i-1] == B[j-1]:
+				dp[i][j] = dp[i-1][j-1] + 1
+			else :
+				dp[i][j] = max (
+						dp[i-1][j],
+						dp[i][j-1]
+					)
+	return dp[N][M]
 def LPS (S):
 	return LCS(S,S[::-1])
 if __name__ == '__main__':
 	S = "AEBCBDA"
-	print (LPS(S))
+	print (len(S) - LPS(S))
