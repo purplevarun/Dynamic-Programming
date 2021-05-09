e474e0bb1a423bda2b21b46474dde2c12fd4f8d2
purplevarun <purplevarun@gmail.com>
2021-05-09 21:53:26 +0530

    lcss-to-do

--- Aditya-Verma-Youtube/20-Longest-Palindromic-Subsequence.cc
+++ Aditya-Verma-Youtube/20-Longest-Palindromic-Subsequence.cc
@@ -11,6 +11,7 @@ int LCS (string A, string B) {
                 dp[i][j] = 0;
         }
     }
+    
     for (int i=1;i<N+1;i++){
         for (int j=1;j<M+1;j++){
             if (A[i-1] == B[j-1])
