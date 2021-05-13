83cdb1d5f5049b955f75980df2c88ec03f1cef9c
purplevarun <purplevarun@gmail.com>
2021-05-14 02:21:05 +0530

    frog1

---
+++ atcoder-dp/1-Frog-1.py
@@ -0,0 +1,21 @@
+# https://atcoder.jp/contests/dp/tasks/dp_a
+'''
+Minimum cost before frog reaches end of array 
+-> Frog can jump from i to i+1 
+-> Frog can jump from i to i+2
+'''
+N = int(input())
+A = [int(X) for X in input().split()]
+
+dp = [None for i in range (N)]
+dp[0] = 0
+dp[1] = abs (A[1] - A[0])
+
+for i in range (2,N):
+	print (dp)
+	dp[i] = min (
+		dp[i-1] + abs (A[i] - A[i-1]),
+		dp[i-2] + abs (A[i] - A[i-2])
+	)
+
+print (dp[-1])
