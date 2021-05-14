ea59b70d1b9d68098b5cadbf5c3e912fa3b3e80e
purplevarun <purplevarun@gmail.com>
2021-05-14 23:11:27 +0530

    .

--- atcoder-dp/3.Vacation.py
+++ atcoder-dp/3.Vacation.py
@@ -1,34 +1,5 @@
 N = int (input())
-A = [None for i in range (N)] 
-B = [None for i in range (N)] 
-C = [None for i in range (N)]
+dp = [[None for i range (3)] for j in range (N)]
 for i in range (N):
-    A[i], B[i], C[i] = [int(x) for x in input().split()]
-dp = []
+    A, B, C = [int (X) for X in input().split()]
 
-# for the first dp element
-val = max (A[i], B[i], C[i])
-act = None
-if val == A[i] : act = 'A'
-elif val == B[i] : act = 'B'
-else : act = 'C'
-dp.append((val,act))     
-
-for i in range (1,N):
-    elements = sorted([A[i],B[i],C[i]])
-    val = elements[-1] 
-    act = None
-    if val == A[i] : act = 'A'
-    elif val == B[i] : act = 'B'
-    else : act = 'C'
-    
-    if act == dp[i-1][1]:
-       val = elements[-2]
-    
-    if val == A[i] : act = 'A'
-    elif val == B[i] : act = 'B'
-    else : act = 'C'
-
-    dp.append((val,act))
-
-print (dp)
