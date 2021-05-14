614752b2a36daf2d5486487d3dfb7534a2576e22
purplevarun <purplevarun@gmail.com>
2021-05-14 13:56:16 +0530

    1

--- atcoder-dp/1.Frog-1.cc
+++ atcoder-dp/1.Frog-1.cc
@@ -2,6 +2,13 @@
 #include <bits/stdc++.h>
 #define int int64_t
 using namespace std;
+/*
+-> Greedy Method Fails
+*/
 int32_t main () {
-
+	int N; cin >> N;
+	int A[N];
+	for (int i=0;i<N;i++) 
+		cin >> A[i];
+	
 }
