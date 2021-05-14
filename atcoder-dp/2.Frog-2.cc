cb7d94ed26ad7e768f049c72ea9d983b9e75c6dc
purplevarun <purplevarun@gmail.com>
2021-05-14 16:43:24 +0530

    memoization-makichut

--- atcoder-dp/2.Frog-2.cc
+++ atcoder-dp/2.Frog-2.cc
@@ -27,13 +27,26 @@ int recursion (int* A, int N, int K, int i = 0, unordered_map <int,int> dp = {})
 	return dp[i] = Min;
 }
 int tabulation (int* A, int N, int K){
-	return -1;
+	int dp[N];
+	for (int i=0;i<N;i++) dp[i] = 1e9;
+	dp[0] = 0;
+	dp[1] = abs (A[1] - A[0]);
+	for (int i=2;i<N;i++){
+		for (int j=i-1,jump=0;j>=0 && jump<K;j--,jump++){
+			dp[i] = min (
+				dp[i],
+				dp[j] + abs (A[i] - A[j]) 
+			);
+		}
+	}
+	return dp[N-1];
 }
 int32_t main () {
 	int N, K; cin >> N >> K;
 	int A[N];
 	for (int i=0;i<N;i++)
 		cin >> A[i];
-	int ans = recursion (A,N,K);
+	// int ans = recursion (A,N,K); // gives TLE
+	int ans = tabulation (A,N,K);
 	cout << ans << '\n';
 }
