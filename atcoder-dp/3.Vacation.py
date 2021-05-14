9b952093c5737e35b694ee30a13e16e7975b9652
purplevarun <purplevarun@gmail.com>
2021-05-14 23:12:32 +0530

    .

--- atcoder-dp/3.Vacation.py
+++ atcoder-dp/3.Vacation.py
@@ -2,4 +2,11 @@ N = int (input())
 dp = [[None for i range (3)] for j in range (N)]
 for i in range (N):
     A, B, C = [int (X) for X in input().split()]
+    # base case 
+    if i == 0:
+        dp[i][0] = A
+        dp[i][1] = B
+        dp[i][2] = C
+        continue
 
+    
