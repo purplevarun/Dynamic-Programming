7bcfa0b467d6d477c86da9688a4495b4f3fe48f8
purplevarun <purplevarun@gmail.com>
2021-05-14 22:47:19 +0530

    doing-3

--- atcoder-dp/3.Vacation.py
+++ atcoder-dp/3.Vacation.py
@@ -2,4 +2,11 @@ N = int (input())
 A = []; B = []; C = []
 for i in range (N):
     A[i], B[i], C[i] = [int(x) for x in input().split()]
+dp = []
+for i in range (N):
+    val = max (A[i], B[i], C[i])
+    act = None
+    if val == A[i] : act = 'A'
+    elif val == B[i] : act = 'B'
+    else : act = 'C'
 
