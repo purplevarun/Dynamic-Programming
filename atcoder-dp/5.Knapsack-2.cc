c9b9d4e98c5e9e25710157bce09657118174c23d
purplevarun <purplevarun@gmail.com>
2021-05-15 20:32:17 +0530

    .

--- atcoder-dp/5.Knapsack-2.cc
+++ atcoder-dp/5.Knapsack-2.cc
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 #define int long long int
 int32_t main () {
@@ -12,10 +12,18 @@ int32_t main () {
 	int dp[N+1][sum+1];
 	for (int i=0;i<N+1;i++) {
 		for (int j=0;j<sum+1;j++){
-			if (i==0 || j==0){
-				dp[i][j] = 0;
-			}
+			dp[i][j] = 1e9;
 		}
 	}
-
+	dp[0][0] = 0;
+	for (int i=1;i<N+1;i++) {
+		for (int j=sum; j>=val[i]; --j){
+			dp[i][j] = min (
+				dp[i-1][j],
+				wt[i-1] + dp[i-1][j-val[i-1]]
+			);		
+			cout << dp[i][j] << ' ';
+		}
+		cout << endl;
+	}
 }
