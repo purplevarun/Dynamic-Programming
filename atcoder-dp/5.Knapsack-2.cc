99d5487b49d4db7328ebc2cfbcb0a258f1015ed7
purplevarun <purplevarun@gmail.com>
2021-05-15 20:41:40 +0530

    .

--- atcoder-dp/5.Knapsack-2.cc
+++ atcoder-dp/5.Knapsack-2.cc
@@ -1,29 +1,15 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 #define int long long int
 int32_t main () {
 	int N, W;
 	cin >> N >> W;
 	int wt[N], val[N];
+	int V = 0;
 	for (int i=0;i<N;i++){
 		cin >> wt[i] >> val[i];
+		V += val[i];
 	}
-	int sum = accumulate (val,val+N,0);
-	int dp[N+1][sum+1];
-	for (int i=0;i<N+1;i++) {
-		for (int j=0;j<sum+1;j++){
-			dp[i][j] = 1e9;
-		}
-	}
-	dp[0][0] = 0;
-	for (int i=1;i<N+1;i++) {
-		for (int j=sum; j>=val[i]; --j){
-			dp[i][j] = min (
-				dp[i-1][j],
-				wt[i-1] + dp[i-1][j-val[i-1]]
-			);		
-			cout << dp[i][j] << ' ';
-		}
-		cout << endl;
-	}
+
+	
 }
