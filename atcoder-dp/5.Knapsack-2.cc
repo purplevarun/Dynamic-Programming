065d3926a17dba4f2a40afe9afef90ec482e09aa
purplevarun <purplevarun@gmail.com>
2021-05-15 19:32:46 +0530

    .

--- atcoder-dp/5.Knapsack-2.cc
+++ atcoder-dp/5.Knapsack-2.cc
@@ -8,25 +8,5 @@ int32_t main () {
 	for (int i=0;i<N;i++){
 		cin >> wt[i] >> val[i];
 	}
-	int dp[100+7][1000+7];
-
-	for (int i=0;i<100+7;i++){
-		for (int j=0;j<1000+7;j++){
-			dp[i][j] = 0;
-		}
-	}
-	for (int i=1;i<N+1;i++){
-		for (int j=1;j<W+1;j++){
-			if (wt[i-1] <= j){
-				dp[i][j] = max (
-					dp[i-1][j],
-					dp[i-1][j-wt[i-1]] + val[i-1]
-				);
-			}
-			else {
-				dp[i][j] = dp[i-1][j];
-			}
-		}
-	}
-	cout << dp[N][W] << endl;
+	
 }
