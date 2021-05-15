06441724e03fbb0a79ebfc00d8675895955e8339
purplevarun <purplevarun@gmail.com>
2021-05-15 21:58:03 +0530

    .

--- atcoder-dp/5.Knapsack-2.py
+++ atcoder-dp/5.Knapsack-2.py
@@ -6,6 +6,6 @@ val = [0 for i in range (N)]
 for i in range (N):
 	wt[i], val[i] = [int(X) for X in input().split()]
 
-M = max (val) + 7
+dp = [[1e9 for i in range (1000000)] for j in range (1000)]
 
-dp = [[None for i in range (M+1)] for j in range (N+1)]
+
