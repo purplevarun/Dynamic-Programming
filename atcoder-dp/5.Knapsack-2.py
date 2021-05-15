30f928a2d04a39036850001dc997c43dbf218e45
purplevarun <purplevarun@gmail.com>
2021-05-15 23:58:45 +0530

    changge

--- atcoder-dp/5.Knapsack-2.py
+++ atcoder-dp/5.Knapsack-2.py
@@ -5,7 +5,26 @@ wt = [0 for i in range (N)]
 val = [0 for i in range (N)]
 for i in range (N):
 	wt[i], val[i] = [int(X) for X in input().split()]
+V = sum (val)
+dp = [[1e9 for i in range (V+1)] for j in range (N+1)]
 
-dp = [[1e9 for i in range (1000000)] for j in range (1000)]
+dp [0][0] = 0
+dp [0][val[0]] = wt[0]
 
+for i in range (1,N):
+    dp[i][0] = 0
+    for j in range (1,V+1):
+        dp[i][j] = dp[i-1][j]
+        dp[i][j] = min (
+            dp[i][j],
+            wt[i]+dp[i-1][j-val[i]]
+        )
 
+answer = None
+
+for i in range (V,-1,-1):
+    if dp[N-1][i] <= W:
+        answer = i
+        break
+
+print (answer)
