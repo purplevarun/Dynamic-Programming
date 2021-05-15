1a30d5726e6bb38a800cc0126ce6df6051b78b33
purplevarun <purplevarun@gmail.com>
2021-05-15 19:34:36 +0530

    .

--- atcoder-dp/5.Knapsack-2.cc
+++ atcoder-dp/5.Knapsack-2.cc
@@ -8,5 +8,14 @@ int32_t main () {
 	for (int i=0;i<N;i++){
 		cin >> wt[i] >> val[i];
 	}
-	
+	int sum = accumulate (val,val+N,0);
+	int dp[N+1][sum+1];
+	for (int i=0;i<N+1;i++) {
+		for (int j=0;j<sum+1;j++){
+			if (i==0 || j==0){
+				dp[i][j] = 0;
+			}
+		}
+	}
+
 }
