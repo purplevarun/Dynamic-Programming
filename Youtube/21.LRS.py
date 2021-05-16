c93286554a48b41fc408bd6d9d80339d9a7cfe47
purplevarun <purplevarun@gmail.com>
2021-05-16 22:29:43 +0530

    21

---
+++ Youtube/21.LRS.py
@@ -0,0 +1,21 @@
+def LCS (A, B):
+	N = len (A)
+	M = len (B)
+	dp = [
+		[
+			0 for i in range (M+1) # M columns
+		]
+		for j in range (N+1) # N rows
+	]
+
+	for i in range (1, N+1):
+		for j in range (1, M+1):
+			if A[i-1] == B[j-1]:
+				dp[i][j] = dp[i-1][j-1] + 1
+			else :
+				dp[i][j] = max (
+					dp[i-1][j],
+					dp[i][j-1]
+				)
+
+	return dp[N][M]
