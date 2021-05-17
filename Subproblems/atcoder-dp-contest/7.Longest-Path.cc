40c49552c6591c9576f8153d7ecd29c210087c5d
purplevarun <purplevarun@gmail.com>
2021-05-17 20:32:01 +0530

    6-lcs-done

--- Subproblems/atcoder-dp-contest/7.Longest-Path.cc
+++ Subproblems/atcoder-dp-contest/7.Longest-Path.cc
@@ -5,5 +5,14 @@ signed main () {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);cout.tie(nullptr);
 	
-	
+	int N, K;
+	scanf ("%lld %lld",&N,&K);
+	int count = 0;
+	int X;
+	for (int i=0;i<N;i++){
+		scanf("%lld",&X);
+		if (X%K==0)
+			count++;
+	}
+	cout << count << '\n';
 }
