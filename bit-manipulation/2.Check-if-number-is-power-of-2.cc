d0041bba756b3ddcb3a3d4569b67bf6de10823ad
purplevarun <purplevarun@gmail.com>
2021-05-19 01:06:06 +0530

    bit-power-of-2

--- bit-manipulation/2.Check-if-number-is-power-of-2.cc
+++ bit-manipulation/2.Check-if-number-is-power-of-2.cc
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 #define int int64_t
+bool isPowerof2 (int N){
+	if (N == 0)
+		return false;
+	return (N & (N-1)) == 0; // main logic
+}
 int32_t main () {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	// N & N-1 has the same bits as N 
 	// except the rightmost set bit
-	
+	for (int i=0;i<1e4;i++)
+		if (isPowerof2(i))
+			cout << i << '\n';
 }
