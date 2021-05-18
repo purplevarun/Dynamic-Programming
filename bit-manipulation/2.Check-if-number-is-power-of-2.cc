5f84ac8c3f24bf16a4d5f161fde5b88eb0bd4262
purplevarun <purplevarun@gmail.com>
2021-05-19 01:02:14 +0530

    bit

---
+++ bit-manipulation/2.Check-if-number-is-power-of-2.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#define int int64_t
+int32_t main () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	// N & N-1 has the same bits as N 
+	// except the rightmost set bit
+	
+}
