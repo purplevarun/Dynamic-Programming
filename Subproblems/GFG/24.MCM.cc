f189c5dfb7b3c144ee59455b1c7fab4917ee1696
purplevarun <purplevarun@gmail.com>
2021-06-01 12:00:50 +0530

    mcm-started

--- Subproblems/GFG/24.MCM.cc
+++ Subproblems/GFG/24.MCM.cc
@@ -1,24 +1,26 @@
 /*
-    FORMAT MCM :
-    Input : String or Array
+	FORMAT MCM :
+	Input : String or Array
 
-    ---------------------------
-      i         k           j
-    
-                func (i,j)
-                  /    \
-                 /      \
-            func(i,k)   func (k+1,j) 
+	---------------------------
+		i         k           j
+	
+							func (i,j)
+								/    \
+							 /      \
+					func(i,k)   func (k+1,j) 
 
-    Base Condition :Smallest Valid Input
-    Base 1 : if (i > j) return 0
-    K -> i to j
-    temp_ans = solve (i,k) + solve (k+1,j) 
+	Base Condition :Smallest Valid Input
+	Base 1 : if (i > j) return 0
+	K -> i to j
+	temp_ans = solve (i,k) + solve (k+1,j) 
 */
-#include <bits/stdc++.h>
-#define int long long int
+#include <iostream>
+#define int long long
 using namespace std;
 signed main () {
-    ios_base::sync_with_stdio(0);cin.tie(0);
-    
+	ios_base::sync_with_stdio (0);cin.tie (0);
+	int N = 5;
+	int A[N] = {40, 20, 30, 10, 30};
+	
 }
