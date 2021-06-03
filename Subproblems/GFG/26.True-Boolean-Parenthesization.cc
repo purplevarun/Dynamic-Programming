edacb3f8b6af7aee70b6f59222280a341a95b691
purplevarun <purplevarun@gmail.com>
2021-06-03 21:25:08 +0530

    26-done

--- Subproblems/GFG/26.True-Boolean-Parenthesization.cc
+++ Subproblems/GFG/26.True-Boolean-Parenthesization.cc
@@ -7,42 +7,48 @@ int MCM (string s, int i, int j, bool istrue) {
 	if (dp[i][j][istrue] != -1)
 		return dp[i][j][istrue];
 	if (i > j)
-		return false;
+		return dp[i][j][istrue] = 0;
 	if (i == j){
+		int ans;
 		if (istrue)
-			return s[i] == 'T';
+			ans = s[i] == 'T';
 		else 
-			return s[i] == 'F';
+			ans = s[i] == 'F';
+		return dp[i][j][istrue] = ans;
 	}
 	int count = 0;
+	int lt, rt, lf, rf;
 	for (int k=i+1;k<j;k+=2) {
-		int left_true = MCM (s,i,k-1,true);
-		int left_false = MCM (s,i,k-1,false);
-		int right_true = MCM (s,k+1,j,true);
-		int right_false = MCM (s,k+1,j,false);
-		if (s[k] == '&'){
-			if (istrue)
-				count += left_true*right_true;
-			else 
-				count += left_false*right_true+
-						 left_true*right_false+
-						 left_false*right_false;
-		}
-		else if (s[k] == '|') {
-			if (istrue)
-				count += left_true*right_false + 
-						 left_true*right_true +
-						 left_false*right_true;
-			else 
-				count += left_false*right_false;
-		}
-		else if (s[k] == '^') {
-			if (istrue)
-				count += left_true*right_false+
-						 left_false*right_true;
-			else 
-				count += left_true*right_true+
-						 left_false*right_false;
+		// first check if dp has it or not
+		lt = dp[i][k-1][1]==-1?MCM(s,i,k-1,1):dp[i][k-1][1];
+		lf = dp[i][k-1][0]==-1?MCM(s,i,k-1,0):dp[i][k-1][0];
+		rt = dp[k+1][j][1]==-1?MCM(s,k+1,j,1):dp[k+1][j][1];
+		rf = dp[k+1][j][0]==-1?MCM(s,k+1,j,0):dp[k+1][j][0];
+		switch (s[k]){
+			case '&' :
+				if (istrue){
+					count += lt*rt;
+				}
+				else {
+					count += lf*rf + lf*rt + lt*rf;
+				}
+				break;
+			case '|':
+				if (istrue){
+					count += lt*rt + lt*rf + lf*rt;
+				}
+				else {
+					count += lf*rf;
+				}
+				break;
+			case '^':
+				if (istrue){
+					count += lt*rf + lf*rt;
+				}
+				else {
+					count += lt*rt + lf*rf;
+				}
+				break;
 		}
 	}
 	return dp[i][j][istrue] = count%1003;
