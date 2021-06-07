3bff54d367d1b608a765a3037ef61f9d3b08225b
purplevarun <purplevarun@gmail.com>
2021-06-07 11:40:28 +0530

    .

---
+++ Subproblems/GFG/27.Scrambled-String.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#define int long long
+using namespace std;
+bool isScramble (string A, string B){
+
+}
+signed main () {
+	ios_base::sync_with_stdio (0);cin.tie (0);
+	string a = "great";
+	string b = "eatgr";
+	int i = 1;
+	int j = a.size() - 1; 
+
+}
