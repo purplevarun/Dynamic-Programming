7887c9067d90d67ba64f6f3402d3690f79a40f58
purplevarun <purplevarun@gmail.com>
2021-06-25 01:44:27 +0530

    27-done

--- Subproblems/GFG/27.Scrambled-String.cc
+++ Subproblems/GFG/27.Scrambled-String.cc
@@ -1,14 +1,31 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #define int long long
 using namespace std;
+unordered_map <string, int> dp;
 bool isScramble (string A, string B){
-
+	string key = A + ", " + B;
+	if (dp.find(key)!=dp.end())
+		return dp[key];
+	if (A == B)
+		return dp[key] = true;
+	if (A.length() != B.length())
+		return dp[key] = false;
+	if (A.length() <= 1)
+		return dp[key] = false;
+	int N = A.length();
+	for (int i=1;i<N;++i){
+		bool cond1 = isScramble(A.substr(0,i), B.substr(N-i,i))&&
+					 isScramble(A.substr(i,N-i), B.substr(0,N-i));
+		bool cond2 = isScramble(A.substr(0,i), B.substr(0,i))&&
+					 isScramble(A.substr(i,N-i), B.substr(i,N-i));
+		if (cond1 || cond2)
+			return dp[key] = true;
+	}
+	return dp[key] = false;
 }
 signed main () {
 	ios_base::sync_with_stdio (0);cin.tie (0);
 	string a = "great";
 	string b = "eatgr";
-	int i = 1;
-	int j = a.size() - 1; 
-
+	cout << isScramble(a, b) << endl;
 }
